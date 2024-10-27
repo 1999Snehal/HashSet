cd9c9c8dd5004204869f3d323c8c6ee5bab72592
1999Snehal <snehallagad84@gmail.com>
2024-10-27 20:12:26 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.HashSet;
+import java.util.Iterator;
+
+class HS
+{
+    public static void main(String arg[])
+    {
+
+        HashSet<Integer> marks = new HashSet<>();
+
+        marks.add(60);
+        marks.add(70);
+        marks.add(80);
+        marks.add(66);
+
+        System.out.println("Hashset :"+marks);
+
+        marks.add(66);
+
+       boolean v1 = marks.remove(60);
+       System.out.println("is 60 removed : "+v1);
+
+
     }
+
 }
